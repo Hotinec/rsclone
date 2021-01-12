321087ce3300b0b07d15664316974061bfaaf31c
Polina04 <baksheyeva0404@gmail.com>
2021-01-12 12:48:14 +0200

    fix: tilemap collision && camera bounds

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,7 +4,7 @@
 import Phaser from 'phaser';
 import terrain from '../assets/map/terrain.png';
 import map from '../assets/map/map.json'
-import earth from '../assets/scorched_earth.png';
+
 import { Player, Zombie, Hero } from '../models';
 import cursor from '../assets/PngItem_2912951.cur';
 
@@ -16,7 +16,6 @@ export class GameScene extends Phaser.Scene {
   preload() {
     this.load.image('tilesets', terrain);
     this.load.tilemapTiledJSON('map', map);
-    this.load.image('earth', earth);
     Player.preload(this);
     Zombie.preload(this);
     Hero.preload(this);
@@ -28,14 +27,11 @@ export class GameScene extends Phaser.Scene {
     const layer1 = map.createLayer('Tile Layer 1', tileset, 0, 0).setDepth(-1);
     const layer2 = map.createLayer('Tile Layer 2', tileset, 0, 0);
     layer2.setCollisionByProperty({ collides: true });
-   
-    // this.matter.world.convertTilemapLayer(layer2)
-    this.add.tilemap('map');
 
     this.player = new Hero({
       scene: this, 
-      x: 3000, 
-      y: 3000, 
+      x: map.widthInPixels / 2, 
+      y: map.heightInPixels / 2, 
       texture: 'knife', 
       frame: 'survivor-idle_knife_0'
     });
@@ -58,7 +54,8 @@ export class GameScene extends Phaser.Scene {
       console.log(this.player)
       this.player.isAttack = true;
     });
-
+    this.physics.add.collider(this.player, layer2, null, null, this);
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     this.cameras.main.startFollow(this.player);
   }
 
