ceba59381ac1b213afbb4192b5608b14a90db7e7
ArseniyXaoc <xaoc190789@gmail.com>
2021-01-13 11:21:45 +0300

    feat: shoot+zombie+physics base

---
+++ src/models/Laser.js
@@ -0,0 +1,43 @@
+import Phaser from 'phaser';
+import laser from '../assets/weapon/laser.png';
+export class LaserGroup extends Phaser.Physics.Arcade.Group {
+    constructor(scene) {
+        super(scene.physics.world, scene);
+
+        this.createMultiple({
+            classType: Laser,
+            frameQuantity: 30,
+            active: false,
+            key: 'laser',
+        })
+    }
+
+    fireLaser(x, y, mouseX, mouseY) {
+        const laser = this.getFirstDead(false);
+        if(laser){
+            laser.fire(x, y, mouseX, mouseY);
+        }
+    }
+}
+
+export class Laser extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, 'laser');
+    }
+
+    static preload(scene) {
+        scene.load.image('laser', laser);
+      }
+
+    fire(x, y, mouseX, mouseY) {
+        this.body.reset(x, y);
+
+        this.setActive(true);
+        this.setVisible(true);
+        let angle = Phaser.Math.Angle.Between(mouseX + this.scene.cameras.main.scrollX, mouseY + this.scene.cameras.main.scrollY, x, y);
+        this.incX = Math.cos(angle);
+        this.incY = Math.sin(angle);
+
+        this.setVelocity(this.incX * -900, this.incY * -900);
+    }
+}
---
+++ src/models/Weapon.js
@@ -0,0 +1,34 @@
+import Phaser from 'phaser';
+import pistol from '../assets/weapon/pistol.png';
+import rifle from '../assets/weapon/rifle.png';
+import shotgun from '../assets/weapon/shotgun.png';
+import { BlobUtils } from '../utils/BlobUtils';
+
+export class Weapon extends Phaser.Physics.Arcade.Sprite {
+    constructor(data) {
+        let {scene, x, y, texture, frame} = data;
+        super(scene, x, y, texture, frame);
+        this.scene.add.existing(this);
+        scene.sys.updateList.add(this);
+        scene.sys.displayList.add(this);
+        this.setScale(0.5);
+        scene.physics.world.enableBody(this);
+        this.setImmovable(false);
+    }
+
+    static preload(scene) {
+        scene.load.image('pistol', pistol);
+        scene.load.image('rifle', rifle);
+        scene.load.image('shotgun', shotgun);    
+    }
+
+    get velocity() {
+        return this.body.velocity;
+      }
+
+    update() {
+        this.rotation += 0.01;
+    }
+
+
+}
